ac420f7d984558828c63f240fa7eb9e4852b58ee
c_zino <celinakhrs@gmail.com>
2026-09-07 21:57:59 +0200

    update error fix

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface PortfolioI18n {
+	getLanguage: () => string;
+	setLanguage: (language: string) => void;
+	getText: (key: string) => unknown;
+	formatLocalTime: (date: Date) => string;
+}
+
+interface Window {
+	portfolioI18n: PortfolioI18n;
+}
